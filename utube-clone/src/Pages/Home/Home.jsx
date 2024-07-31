eb57d01b75c19cd12f81749b7e0285bf638e0791
vanshikaasharma11 <vanshikaasharma124@gmail.com>
2024-07-31 21:47:03 +0530

    First Commit

---
+++ utube-clone/src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+
+const Home = ({sidebar}) => {
+  return (
+    <>
+      <Sidebar sidebar={sidebar}/>
+
+    </>
+  )
+}
+
+export default Home
